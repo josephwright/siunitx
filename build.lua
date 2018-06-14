ce984f47542fc2f51137a18a97e45a776fd4d522
Joseph Wright <joseph.wright@morningstar2.co.uk>
2018-06-14 11:00:24 +0100

    Not 'release'

--- build.lua
+++ build.lua
@@ -24,7 +24,7 @@ function update_tag(file,content,tagname,tagdate)
 end
 
 function tag_hook(tagname)
-  os.execute('git commit -a -m "Step release tag"')
+  os.execute('git commit -a -m "Step tag"')
 --  os.execute('git tag -a -m "" ' .. tagname)
 end
 
