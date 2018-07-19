e0a7df59497196ee1cffa8b70c30e40ce7c2cd3e
Unknown <joseph.wright@morningstar2.co.uk>
2018-07-19 18:51:06 +0100

    Better tagging pattern

--- build.lua
+++ build.lua
@@ -18,7 +18,7 @@ typesetfiles = {"*.tex"}
 -- Detail how to set the version automatically
 function update_tag(file,content,tagname,tagdate)
   return string.gsub(content,
-    "\n\\ProvidesExplPackage %{siunitx%} %{%d%d%d%d%-%d%d%-%d%d%} %{%d%.%d%w?%}\n",
+    "\n\\ProvidesExplPackage %{siunitx%} %{[^}]+%} %{[^}]+%}\n",
     "\n\\ProvidesExplPackage {siunitx} {"
       .. tagdate .. "} {" .. string.gsub(tagname, "^v", "") .. "}\n")
 end
