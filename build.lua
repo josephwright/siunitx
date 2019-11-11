a7d027f60c3715ceef61d7754d2b5102f22835d1
Joseph Wright <joseph.wright@morningstar2.co.uk>
2019-11-11 17:39:40 +0000

    Save another cycle

--- build.lua
+++ build.lua
@@ -9,9 +9,6 @@ module = "siunitx"
 -- Release a TDS-style zip
 packtdszip = true
 
--- Two configs
-checkconfigs = {"build","config-pdfstring"}
-
 -- Typeset only the .tex files
 typesetfiles = {"*.tex"}
 
--- config-pdfstring.lua
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env texlua
-
--- Build script for "siunitx" PDF strings
-
-checkruns   = 2
-testfiledir = "testfiles-pdfstring"
